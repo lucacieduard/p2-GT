32df2fdb29c6fbf06c5a967b4bfc5da1ae2431a1
lucacieduard <eduardlucaci2002@yahoo.com>
2022-12-22 15:48:44 +0200

    new search added

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,10 +1,9 @@
-import { fetcher } from 'utils/api';
+import { fetcher } from "utils/api";
 
 const getSearchMovieUrl = (terms) =>
-  `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${terms}`;
+	`https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${terms}`;
 
 export default async function handler(req, res) {
-  const results = await fetcher(getSearchMovieUrl(req.query.terms));
-
-  res.status(200).json(results);
+	const results = await fetcher(getSearchMovieUrl(req.query.terms));
+	res.status(200).json(results);
 }
--- pages/search.js
+++ pages/search.js
@@ -1,107 +1,148 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
-import useSWR from 'swr';
+import { useEffect, useState } from "react";
+import {} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import useSWR from "swr";
 import {
-  Input,
-  IconButton,
-  Container,
-  UnorderedList,
-  ListItem,
-  Progress,
-  Text,
-  InputGroup,
-  InputRightElement,
-  VStack,
-  Button,
-  Badge,
-} from '@chakra-ui/react';
-import { SearchIcon } from '@chakra-ui/icons';
-import Layout from 'components/Layout';
+	Input,
+	IconButton,
+	Container,
+	UnorderedList,
+	ListItem,
+	Progress,
+	Text,
+	InputGroup,
+	InputRightElement,
+	VStack,
+	Button,
+	Badge,
+	Card,
+	CardHeader,
+	CardBody,
+	CardFooter,
+	Heading,
+	Stack,
+	Image,
+} from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
+import Layout from "components/Layout";
+import { buildImageUrl } from "utils/api";
 
 function SearchBar() {
-  const router = useRouter();
-  const { terms } = router.query;
-  const [text, setText] = useState('');
+	const router = useRouter();
+	const { terms } = router.query;
+	const [text, setText] = useState("");
 
-  // Update text input when route changes (ex when user goes back/forward)
-  useEffect(() => {
-    setText(terms || '');
-  }, [terms]);
+	// Update text input when route changes (ex when user goes back/forward)
+	useEffect(() => {
+		setText(terms || "");
+	}, [terms]);
 
-  // Update router history if a search was performed
-  const handleSearch = (event) => {
-    event.preventDefault();
-    if (text !== terms) {
-      router.push(`/search/?terms=${text}`, undefined, { shallow: true });
-    }
-  };
+	// Update router history if a search was performed
+	const handleSearch = (event) => {
+		event.preventDefault();
+		if (text !== terms) {
+			router.push(`/search/?terms=${text}`, undefined, { shallow: true });
+		}
+	};
 
-  return (
-    <InputGroup as="form" onSubmit={handleSearch}>
-      <Input
-        placeholder="Search for a movie..."
-        value={text}
-        onChange={(event) => setText(event.target.value)}
-      />
-      <InputRightElement>
-        <IconButton
-          aria-label="Search for a movie"
-          icon={<SearchIcon />}
-          type="submit"
-        />
-      </InputRightElement>
-    </InputGroup>
-  );
+	return (
+		<InputGroup as="form" onSubmit={handleSearch}>
+			<Input
+				placeholder="Search for a movie..."
+				value={text}
+				onChange={(event) => setText(event.target.value)}
+			/>
+			<InputRightElement>
+				<IconButton
+					aria-label="Search for a movie"
+					icon={<SearchIcon />}
+					type="submit"
+				/>
+			</InputRightElement>
+		</InputGroup>
+	);
 }
 function SearchResults() {
-  const { terms } = useRouter().query;
-  const { data, error } = useSWR(terms && `/api/search?terms=${terms}`);
+	const { terms } = useRouter().query;
+	const { data, error } = useSWR(terms && `/api/search?terms=${terms}`);
 
-  if (!terms) {
-    return <Text>Type some terms and submit for a quick search</Text>;
-  }
-  if (error) {
-    return (
-      <Text color="red">
-        Error fetching movies for {terms}: {JSON.stringify(error)}
-      </Text>
-    );
-  }
-  if (!data) {
-    return <Progress size="xs" isIndeterminate />;
-  }
-  if (!data.results.length) {
-    return <Text>No results</Text>;
-  }
-  return (
-    <UnorderedList stylePosition="inside">
-      {data.results.map(({ id, title, release_date }) => (
-        <ListItem key={id}>
-          <Link href={`/movies/${id}`} passHref legacyBehavior>
-            <Button
-              as="a"
-              variant="link"
-              rightIcon={<Badge>{release_date}</Badge>}
-            >
-              <Text as="span">{title} </Text>
-            </Button>
-          </Link>
-        </ListItem>
-      ))}
-    </UnorderedList>
-  );
+	if (!terms) {
+		return <Text>Type some terms and submit for a quick search</Text>;
+	}
+	if (error) {
+		return (
+			<Text color="red">
+				Error fetching movies for {terms}: {JSON.stringify(error)}
+			</Text>
+		);
+	}
+	if (!data) {
+		return <Progress size="xs" isIndeterminate />;
+	}
+	if (!data.results.length) {
+		return <Text>No results</Text>;
+	}
+	return (
+		<UnorderedList stylePosition="inside">
+			{data.results.map((movie) => {
+				if (movie.overview.length > 1) {
+					return (
+						<Card
+							key={movie.id}
+							direction={{ base: "column", sm: "row" }}
+							overflow="hidden"
+							variant="outline"
+							m={"20px"}
+						>
+							<Image
+								objectFit="cover"
+								maxW={{ base: "100%", sm: "200px" }}
+								src={
+									movie.poster_path
+										? buildImageUrl(movie.poster_path, "w200")
+										: "fallback-img.png"
+								}
+								alt="Caffe Latte"
+							/>
+
+							<Stack>
+								<CardBody>
+									<Heading size="md">{movie.title}</Heading>
+
+									<Text py="5">{movie.overview}</Text>
+								</CardBody>
+
+								<CardFooter>
+									<Link
+										href={`/movies/${movie.id}`}
+										passHref
+										legacyBehavior
+										key={movie.id}
+									>
+										<Button variant="solid" colorScheme="blue">
+											More info
+										</Button>
+									</Link>
+								</CardFooter>
+							</Stack>
+						</Card>
+					);
+				}
+			})}
+		</UnorderedList>
+	);
 }
 
 export default function Search() {
-  return (
-    <Layout title="Search">
-      <Container>
-        <VStack spacing={4} align="stretch">
-          <SearchBar />
-          <SearchResults />
-        </VStack>
-      </Container>
-    </Layout>
-  );
+	return (
+		<Layout title="Search">
+			<Container>
+				<VStack spacing={4} align="stretch">
+					<SearchBar />
+					<SearchResults />
+				</VStack>
+			</Container>
+		</Layout>
+	);
 }
